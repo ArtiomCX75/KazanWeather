e3f5936c64dd2944f4bc4687079a29b7083d6b26
Artem Fokin <faa1192@gmail.com>
2025-06-22 13:46:30 +0300

    update version to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.faa1192.kazanweather"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "3"
+        versionCode = 4
+        versionName = "4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
